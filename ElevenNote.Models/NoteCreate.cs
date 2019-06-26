975a9a7fbeedf64356dce1b668cf40a11014ac50
masongarard <masongarard@gmail.com>
2019-06-26 13:45:10 -0400

    Notes Tab

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2,ErrorMessage ="More than 2 characters, please.")]
+        [MaxLength(100,ErrorMessage ="Oh come on, really? That's way too many characters!")]
+        public string Title { get; set; }
+
+        [MaxLength(8000)]
+        public string Content { get; set; }
+    }
+}
--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -14,7 +14,24 @@ namespace ElevenNote.WebMVC.Controllers
         public ActionResult Index()
         {
             var model = new NoteListItem[0];
+            return View(model);
+        }
+
+        //GET
+        public ActionResult Create()
+        {
             return View();
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(NoteCreate model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
         }
     }
 }
